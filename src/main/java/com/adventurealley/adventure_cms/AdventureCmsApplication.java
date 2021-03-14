9df76a4cf6bb7b88f8ed17063c497f790370607f
Kira Nim <kiras.nim@gmail.com>
2021-03-14 13:58:59 +0100

    initial commit

---
+++ src/main/java/com/adventurealley/adventure_cms/AdventureCmsApplication.java
@@ -0,0 +1,13 @@
+package com.adventurealley.adventure_cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdventureCmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdventureCmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/adventurealley/adventure_cms/AdventureCmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adventurealley.adventure_cms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdventureCmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
